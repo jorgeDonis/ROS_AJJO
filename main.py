635f3c3b3bb3698699322f7101b5d327f3b04818
Jorge Donis <jdda2@alu.ua.es>
2021-03-27 21:44:10 +0100

    alex entrena toxicidad fuera mala vibra f_

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import system
 from typing import ChainMap
 import numpy as np
 import tensorflow as tf
@@ -11,49 +12,29 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras import layers
 
 IMG_WIDTH = 128
-IMG_HEIGHT = 64
+IMG_HEIGHT = 128
 
 BATCH_SIZE = 8
-EPOCHS = 30
+EPOCHS = 20
 
 def preprocess_img(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = np.delete(img, slice(1, 100), 0)
-    img = np.delete(img, slice(290, img.shape[0]), 0)
+    img = np.delete(img, slice(1, 90), 0)
+    img = np.delete(img, slice(300, img.shape[0]), 0)
     img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT), interpolation=cv2.INTER_LANCZOS4)
     img = np.reshape(img, (img.shape[0], img.shape[1], 1))
     img = img / 255
-    # img = img.astype(np.float32) / 255.0
     return img
-
-def get_model():
-    inputs = tf.keras.layers.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 1), name='opencv_original_imgs')
-    x = tf.keras.layers.Conv2D(9, (4, 4), padding='same') (inputs)
-    x = tf.keras.layers.MaxPool2D(pool_size=(3, 3)) (x)
-    x = tf.keras.layers.Conv2D(3, (3, 3)) (x)
-    x = tf.keras.layers.Conv2D(3, (3, 3)) (x)
-    x = tf.keras.layers.Conv2D(5, (2, 2)) (x)
-    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2)) (x)
-    x = tf.keras.layers.Conv2D(5, (2, 2), padding='same') (x)
-    x = tf.keras.layers.MaxPool2D(pool_size=(2, 2)) (x)
-    x = tf.keras.layers.Flatten() (x)
-    x = tf.keras.layers.Dense(64) (x)
-    x = tf.keras.layers.Dense(32) (x)
-    outputs = tf.keras.layers.Dense(3, activation='relu') (x)
-    model = tf.keras.Model(inputs, outputs)
-    model.compile(optimizer='adam', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-
-    return model
 
 
 def cnn_model():
 
     model = tf.keras.Sequential()
 
+    model.add(layers.Input(shape=(IMG_HEIGHT, IMG_WIDTH, 1)))
     model.add(layers.Conv2D(6, (5, 5)))
     model.add(layers.Activation('relu'))
-    model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+    model.add(layers.MaxPooling2D(pool_size=(4, 4)))
 
     model.add(layers.Conv2D(16, (5, 5)))
     model.add(layers.Activation('relu'))
@@ -129,14 +110,71 @@ def show_img(img, label):
         plt.title("STEER_RIGHT")
     plt.show()
 
+def generator():
+    X = []
+    Y = []
+    i = 0
+    for filename in (glob.glob("./dataset/*")):
+        regex = r"([A-Z_]+)_\d+\.jpg"
+        category = re.findall(regex, filename, re.MULTILINE)[0]
+        img = cv2.imread(filename)
+        X.append(img)
+        if (category == "NO_ACTION"):
+            Y.append(np.array([1, 0, 0]))
+        elif (category == "STEER_LEFT"):
+            Y.append(np.array([0, 1, 0]))
+        elif (category == "STEER_RIGHT"):
+            Y.append(np.array([0, 0, 1]))
+        i += 1
+        if i == BATCH_SIZE:
+            X = [preprocess_img(x) for x in X]
+            X, Y = shuffle_ds(X, Y)
+            X, Y = balance_ds(X, Y)
+            if (len(Y) != 0):
+                yield np.array(X), np.array(Y)
+            X = []
+            Y = []
+            i = 0
+
+def balance_ds_directory(directory):
+    no_action_filenames = []
+    steer_left_filenames = []
+    steer_right_filenames = []
+    for filename in (glob.glob("./dataset/*")):
+        regex = r"([A-Z_]+)_\d+\.jpg"
+        category = re.findall(regex, filename, re.MULTILINE)[0]
+        if (category == "NO_ACTION"):
+            no_action_filenames.append(filename)
+        elif (category == "STEER_LEFT"):
+            steer_left_filenames.append(filename)
+        elif (category == "STEER_RIGHT"):
+            steer_right_filenames.append(filename)
+    samples_per_cat = min(len(no_action_filenames), len(steer_left_filenames))
+    samples_per_cat = min(len(steer_right_filenames), samples_per_cat)
+    random.shuffle(no_action_filenames)
+    random.shuffle(steer_left_filenames)
+    random.shuffle(steer_right_filenames)
+    for i in range(0, len(no_action_filenames) - samples_per_cat):
+        system("rm " + no_action_filenames[i])
+    for i in range(0, len(steer_left_filenames) - samples_per_cat):
+        system("rm " + steer_left_filenames[i])
+    for i in range(0, len(steer_right_filenames) - samples_per_cat):
+        system("rm " + steer_right_filenames[i])
+
+
+
+# balance_ds_directory("dataset")
 X, Y = load_dataset()
-X, Y = shuffle_ds(X, Y)
-X, Y = balance_ds(X, Y)
+# X, Y = shuffle_ds(X, Y)
+# X, Y = balance_ds(X, Y)
 
 # for i in range(0, len(X)):
 #     show_img(X[i], Y[i])
 
-# model = get_model()
 model = cnn_model()
+# print(model.summary())
+
+# gen = generator()
+
 model.fit(X, Y, batch_size = BATCH_SIZE, epochs = EPOCHS)
 model.save('tf_model_driving.h5', include_optimizer=False)
