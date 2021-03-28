b3e6f0782aef9eb3e2a10b45e87e370e54758138
AlehandroG <47591551+AlehandroG@users.noreply.github.com>
2021-03-28 19:58:49 +0200

    No hace falta subirlo y tal

--- build/grabador_datos/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gnomo/Desktop/ROS_AJJO/src/grabador_datos
-# Build directory: /home/gnomo/Desktop/ROS_AJJO/build/grabador_datos
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
