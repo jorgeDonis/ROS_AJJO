bb151291fcc654cc50d5f8b58de347f081985f5d
AlehandroG <47591551+AlehandroG@users.noreply.github.com>
2021-03-28 19:58:49 +0200

    No hace falta subirlo y tal

--- build/ejemplogazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gnomo/Desktop/ROS_AJJO/src/ejemplogazebo
-# Build directory: /home/gnomo/Desktop/ROS_AJJO/build/ejemplogazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
