d38d6310e95533f24d6d9f98cf0adcbc8a68f1b3
Jorge Donis del Álamo <jdda2@alu.ua.es>
2021-04-25 18:45:48 +0200

    empezando a entender pcl

--- src/get_pointclouds/src/node.cpp
+++ src/get_pointclouds/src/node.cpp
@@ -5,6 +5,8 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/pfh.h>
 
 pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
 
@@ -33,7 +35,33 @@ void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
 
 	cout << "Puntos tras VG: " << cloud_filtered->size() << endl;
 
+	// Object for storing the normals.
+	pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
+	// Object for storing the PFH descriptors for each point.
+	pcl::PointCloud<pcl::PFHSignature125>::Ptr descriptors(new pcl::PointCloud<pcl::PFHSignature125>());
+
+	// Estimate the normals.
+	pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normalEstimation;
+	normalEstimation.setInputCloud(cloud_filtered);
+	normalEstimation.setRadiusSearch(0.03);
+	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+	normalEstimation.setSearchMethod(kdtree);
+	normalEstimation.compute(*normals);
+
+	// PFH estimation object.
+	pcl::PFHEstimation<pcl::PointXYZRGB, pcl::Normal, pcl::PFHSignature125> pfh;
+	pfh.setInputCloud(cloud_filtered);
+	pfh.setInputNormals(normals);
+	pfh.setSearchMethod(kdtree);
+	// Search radius, to look for neighbors. Note: the value given here has to be
+	// larger than the radius used to estimate the normals.
+	pfh.setRadiusSearch(0.05);
+
+	pfh.compute(*descriptors);
+
 	visu_pc = cloud_filtered;
+
+	cout << "tamanyo vector: " << descriptors->size() << "\n";
 }
 
 int main(int argc, char** argv)
