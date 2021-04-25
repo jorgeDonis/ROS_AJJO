7e048c90e982580a8a59978098658d88659e9ae8
Jorge Donis del Álamo <jdda2@alu.ua.es>
2021-04-25 17:25:07 +0200

    nueva branch

--- src/grabador_datos/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/jorge/catkin_ws/src/grabador_datos/include/grabador_datos".split(';') if "/home/jorge/catkin_ws/src/grabador_datos/include/grabador_datos" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "grabador_datos"
-PROJECT_SPACE_DIR = "/home/jorge/catkin_ws/src/grabador_datos/build/devel"
-PROJECT_VERSION = "0.0.0"
