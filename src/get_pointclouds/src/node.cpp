d16c892f3647e549040904cbd1113c40de51cbcf
Jorge Donis del Álamo <jdda2@alu.ua.es>
2021-04-26 13:05:07 +0200

    empezamos con el algoritmo

--- src/get_pointclouds/src/node.cpp
+++ src/get_pointclouds/src/node.cpp
@@ -1,80 +1,31 @@
+#include "MapBuilder.hpp"
+
 #include <ros/ros.h>
 #include <pcl_ros/point_cloud.h>
-#include <pcl/point_types.h>
-#include <boost/foreach.hpp>
-#include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include <pcl/filters/voxel_grid.h>
-#include <pcl/features/normal_3d.h>
-#include <pcl/features/pfh.h>
 
-pcl::PointCloud<pcl::PointXYZRGB>::Ptr visu_pc (new pcl::PointCloud<pcl::PointXYZRGB>);
+using PointCloud = pcl::PointCloud<pcl::PointXYZRGB>;
 
-void simpleVis ()
+static PointCloud::Ptr visu_pc(new PointCloud);
+
+void simpleVis()
 {
-  	pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
-	while(!viewer.wasStopped())
+  	pcl::visualization::CloudViewer viewer ("Reconstrucción 3D");
+	while (!viewer.wasStopped())
 	{
-	  viewer.showCloud (visu_pc);
+	  viewer.showCloud(visu_pc);
 	  boost::this_thread::sleep(boost::posix_time::milliseconds(1000));
 	}
-
-}
-
-void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr& msg)
-{
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>(*msg));
-	pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
-
-	cout << "Puntos capturados: " << cloud->size() << endl;
-
-	pcl::VoxelGrid<pcl::PointXYZRGB > vGrid;
-	vGrid.setInputCloud (cloud);
-	vGrid.setLeafSize (0.05f, 0.05f, 0.05f);
-	vGrid.filter (*cloud_filtered);
-
-	cout << "Puntos tras VG: " << cloud_filtered->size() << endl;
-
-	// Object for storing the normals.
-	pcl::PointCloud<pcl::Normal>::Ptr normals(new pcl::PointCloud<pcl::Normal>);
-	// Object for storing the PFH descriptors for each point.
-	pcl::PointCloud<pcl::PFHSignature125>::Ptr descriptors(new pcl::PointCloud<pcl::PFHSignature125>());
-
-	// Estimate the normals.
-	pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normalEstimation;
-	normalEstimation.setInputCloud(cloud_filtered);
-	normalEstimation.setRadiusSearch(0.03);
-	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr kdtree(new pcl::search::KdTree<pcl::PointXYZRGB>);
-	normalEstimation.setSearchMethod(kdtree);
-	normalEstimation.compute(*normals);
-
-	// PFH estimation object.
-	pcl::PFHEstimation<pcl::PointXYZRGB, pcl::Normal, pcl::PFHSignature125> pfh;
-	pfh.setInputCloud(cloud_filtered);
-	pfh.setInputNormals(normals);
-	pfh.setSearchMethod(kdtree);
-	// Search radius, to look for neighbors. Note: the value given here has to be
-	// larger than the radius used to estimate the normals.
-	pfh.setRadiusSearch(0.05);
-
-	pfh.compute(*descriptors);
-
-	visu_pc = cloud_filtered;
-
-	cout << "tamanyo vector: " << descriptors->size() << "\n";
 }
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "sub_pcl");
-  ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe<pcl::PointCloud<pcl::PointXYZRGB> >("/camera/depth/points", 1, callback);
-
-  boost::thread t(simpleVis);
-
-  while(ros::ok())
-  {
-	ros::spinOnce();
-  }
-
+  	ros::init(argc, argv, "sub_pcl");
+  	ros::NodeHandle nh;
+	boost::thread t(simpleVis);
+	MapBuilder map_builder("point_cloud_messages.bag", visu_pc);
+	map_builder.build_map();
+	std::cout << "Terminado reconstrucción 3D!" << std::endl;
+	while (ros::ok())
+		ros::spinOnce();
 }
