7ed36b42b5367301b65956f450cde34b06f7cbb1
Jorge Donis del Álamo <jdda2@alu.ua.es>
2021-05-09 18:07:41 +0200

    empezando a refactorizar

--- src/get_pointclouds/src/node.cpp
+++ src/get_pointclouds/src/node.cpp
@@ -15,12 +15,12 @@ int main(int argc, char** argv)
 
 		MapBuilder map_builder1("/home/jorge/catkin_ws/point_cloud_messages.bag",
 		0.005,		//Voxel grid leaf size
-		0.03,		//FFPH search radius
+		0.04,		//FFPH search radius
 		0.001,		//SIFT3D minimmum scale
 		9,			//SIFT3D nº octaves
 		11,			//SIFT3D number of scales per octave
 		0.001,		//SIFT3D minimmum contrast
-		0.0009		//RANSAC inlier threshold
+		0.01		//RANSAC inlier threshold
 		);
 		map_builder1.build_map();
 
